222d296ac87f1abf3e5b4a28125aa97b5451c1e0
abvinnovator <brahmavamsi1234@gmail.com>
2024-04-07 23:59:14 +0530

    updated next-config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
       domains: ["lh3.googleusercontent.com"],
     },
+    experimental: {
+      missingSuspenseWithCSRBailout: false,
+    },
     webpack(config) {
       config.experiments = {
         ...config.experiments,
