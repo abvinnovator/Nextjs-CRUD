182ffff00f65a47b40992a6854bff7fdb53ed889
abvinnovator <brahmavamsi1234@gmail.com>
2024-04-07 23:44:18 +0530

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import '../styles/globals.css'
+
+import Nav from "../componenets/Nav";
+import Provider from "../componenets/Provider";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
